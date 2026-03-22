a678b20a7b9fdb34c18ba3aff87b36d2648d8565
jruegoes <jure.renusa@proton.me>
2026-03-22 17:34:39 +0100

    321

--- src/routes/_authenticated.tsx
+++ src/routes/_authenticated.tsx
@@ -48,7 +48,7 @@ function AuthenticatedLayout() {
         <main className="flex-1 overflow-y-auto px-4 pb-10 pt-6 sm:px-6 sm:pt-8 lg:px-10">
           <Outlet />
         </main>
-        <div className="fixed bottom-4 left-4 z-50">
+        <div className="fixed bottom-4 right-4 z-50">
           <ThemeToggle />
         </div>
       </div>
@@ -79,7 +79,7 @@ function AuthenticatedLayout() {
         </button>
       </div>
       <Outlet />
-      <div className="fixed bottom-4 left-4 z-50">
+      <div className="fixed bottom-4 right-4 z-50">
         <ThemeToggle />
       </div>
     </div>
--- src/routes/login.tsx
+++ src/routes/login.tsx
@@ -126,7 +126,7 @@ function LoginPage() {
 
 
       </div>
-      <div className="fixed bottom-4 left-4 z-50">
+      <div className="fixed bottom-4 right-4 z-50">
         <ThemeToggle />
       </div>
     </main>
